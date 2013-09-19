e4c1eed579f743fdad03fec8feb6f8071b0a3c06
dimhold <dimhold@gmail.com>
2013-09-19 15:50:37 +0300

    Add config library

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var http = require("http");
+var CONF = require("config");
 
 http.createServer(function (request, response) {
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("Foodex");
     response.end();
-}).listen(8888);
+}).listen(CONF.app.port);
