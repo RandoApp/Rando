142172e27f337d09596c84b3e78aedcd4680801b
dimhold <dimhold@gmail.com>
2013-10-03 00:06:16 +0300

    Add tests for User already exists error and for positive correct and expected behaviour

--- test/service/account.js
+++ test/service/account.js
@@ -26,9 +26,33 @@ describe('Account service', function () {
 	});
 
 	it('Correct email and password should not return Error', function () {
+	    var email = "email@mail.com";
+
+	    var mongoose = require("mongoose");
+	    sinon.stub(mongoose.Model.prototype, "save", function (callback) {callback();});
+	    sinon.stub(mongoose.Model, "findOne", function (email, callback) {
+		callback();
+	    });
+
 	    account.registerByEmailAndPassword("email@mail.com", "password", function (err) {
 		should.not.exist(err);
 	    });
+	});
+
+	it('User already exist', function () {
+	    var email = "email@mail.com";
+
+	    mongoose = require("mongoose");
+	    mongoose.Model.findOne.restore();
+	    sinon.stub(mongoose.Model, "findOne", function (email, callback) {
+		callback(null, {user: "some user"});
+	    });
+
+	    account.registerByEmailAndPassword("email@mail.com", "password", function (err) {
+		should.exist(err);
+		err.should.have.property("message", "User already exists");
+	    });
+
 	});
 
     });
