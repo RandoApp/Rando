7876db227706ff28e246e412a81c7eda490e6002
dimhold <dimhold@gmail.com>
2013-10-11 00:33:24 +0300

    Add util tests

---
+++ test/util/util.js
@@ -0,0 +1,54 @@
+var should = require("should");
+var sinon = require("sinon");
+var util = require("../../src/util/util");
+var crypto = require("crypto");
+
+describe('Util.', function () {
+    describe('generateUniqueName.', function () {
+	it('Error from crypto should bubble up', function (done) {
+	    var error = "Error in crypto";
+	    sinon.stub(crypto, "pseudoRandomBytes", function (length, callback) {
+		callback(new Error(error));
+		crypto.pseudoRandomBytes.restore();
+	    });
+
+	    util.generateUniqueName(function (err) {
+		should.exist(err);
+		err.should.have.property("message", error);
+		done();
+	    });
+	});
+
+	it('Successful unique name', function (done) {
+	    util.generateUniqueName(function (err, name) {
+		should.not.exist(err);
+		name.should.match(/^[\w\d]+$/);
+		done();
+	    });
+	});
+    });
+
+    describe('generateFoodName.', function () {
+	it('Error bubble up', function (done) {
+	    var error = "Error when generate unique name";
+	    sinon.stub(util, "generateUniqueName", function (callback) {
+		callback(new Error(error));
+		util.generateUniqueName.restore();
+	    });
+
+	    util.generateFoodName(function (err, fullPath) {
+		should.exist(err);
+		err.should.have.property("message", error);
+		done();
+	    });
+	});
+
+	it('Full path is correct', function (done) {
+	    util.generateFoodName(function (err, fullPath) {
+		should.not.exist(err);
+		fullPath.should.match(/^[\w\d\/]*[\w\d]+\/[\w\d]+\.png$/);
+		done();
+	    });
+	});
+    });
+});
