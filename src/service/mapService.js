bcb4cb70022db43f6b4c15f80a9008ffe8044c95
dimhold <dimhold@gmail.com>
2014-03-27 15:50:42 +0300

    Add log messages for mapService

--- src/service/mapService.js
+++ src/service/mapService.js
@@ -8,31 +8,37 @@ var crypto = require("crypto");
 module.exports =  {
     cities: null,
     locationToMapUrlSync: function (latitude, longitude) {
-		if (!this.cities) this.loadCitiesJson();
+	logger.debug("[mapService.locationToMapUrlSync] latitude: ", latitude, " longitude: ", longitude);
+	if (!this.cities) this.loadCitiesJson();
 
-		var city = this.findNearestCity(latitude, longitude);
-		var tileName = this.generateTileName(city);
-		return config.app.url + config.app.static.folder.map + tileName + "." + config.app.static.file.ext;
+	var city = this.findNearestCity(latitude, longitude);
+	logger.debug("[mapService.locationToMapUrlSync] Found nearest city: ", city);
+	var tileName = this.generateTileName(city);
+	logger.debug("[mapService.locationToMapUrlSync] generate tileName: ", tileName);
+	var mapUrl = config.app.url + config.app.static.folder.map + tileName + "." + config.app.static.file.ext;
+	logger.debug("[mapService.locationToMapUrlSync] mapUrl: ", mapUrl);
+	return mapUrl;
 	},
 	findNearestCity: function (latitude, longitude) {
-		var minDistance = Number.MAX_VALUE;
-		var city = this.cities[0];
-		for (var i = 0; i < this.cities.length; i++) {
-			var distance = Math.sqrt(Math.pow(latitude - this.cities[i].latitude, 2) + Math.pow(longitude - this.cities[i].longitude, 2));
-			if (distance < minDistance) {
-				city = this.cities[i];
-				minDistance = distance;
-			}
-		}
-		return city;
+	    var minDistance = Number.MAX_VALUE;
+	    var city = this.cities[0];
+	    for (var i = 0; i < this.cities.length; i++) {
+		    var distance = Math.sqrt(Math.pow(latitude - this.cities[i].latitude, 2) + Math.pow(longitude - this.cities[i].longitude, 2));
+		    if (distance < minDistance) {
+			    city = this.cities[i];
+			    minDistance = distance;
+		    }
+	    }
+	    return city;
 	},
 	generateTileName: function (city) {
-		return crypto.createHash("md5").update(city.name + city.longitude + city.latitude).digest("hex");
+	    return crypto.createHash("md5").update(city.name + city.longitude + city.latitude).digest("hex");
 	},
 	loadCitiesJson: function () {
-		if (!this.cities) {
-			this.cities = JSON.parse(fs.readFileSync(config.app.citiesJson));
-		}
+	    if (!this.cities) {
+		this.cities = JSON.parse(fs.readFileSync(config.app.citiesJson));
+		logger.debug("[mapService.loadCitiesJson] cities loaded");
+	    }
 	}
 }
 
