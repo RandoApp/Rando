2d2853d21d62d2e1ab83e636c7d16d5678c28756
Viktar Patotski <xp.vit.blr@gmail.com>
2016-08-02 15:23:21 +0300

    Fix codacy issues

--- src/util/password.js
+++ src/util/password.js
@@ -3,14 +3,14 @@ var crypto = require("crypto");
 var config = require("config");
 
 module.exports = {
-  generateHashForPassword: function (email, password) {
+  generateHashForPassword (email, password) {
     logger.data("[password.generateHashForPassword, ", email, "] Try generate hash.");
     var sha1sum = crypto.createHash("sha1");
     sha1sum.update(password + email + config.app.secret);
     return sha1sum.digest("hex");
   },
-  isPasswordCorrect: function (password, user) {
+  isPasswordCorrect (password, user) {
     logger.data("[userService.isPasswordCorrect, ", user.email, "] Try compare passwords: ", user.password, " == ", this.generateHashForPassword(user.email, password));
-    return user.password == this.generateHashForPassword(user.email, password);
+    return user.password === this.generateHashForPassword(user.email, password);
   }
 };
