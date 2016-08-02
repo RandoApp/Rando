335e05fc5e4e7283cb552a29d007b5d356251d2a
Viktar Patotski <xp.vit.blr@gmail.com>
2016-08-02 15:23:53 +0300

    Fix codacy issues

--- src/util/password.js
+++ src/util/password.js
@@ -10,7 +10,7 @@ module.exports = {
     return sha1sum.digest("hex");
   },
   isPasswordCorrect (password, user) {
-    logger.data("[userService.isPasswordCorrect, ", user.email, "] Try compare passwords: ", user.password, " == ", this.generateHashForPassword(user.email, password));
+    logger.data("[userService.isPasswordCorrect, ", user.email, "] Try compare passwords: ", user.password, " === ", this.generateHashForPassword(user.email, password));
     return user.password === this.generateHashForPassword(user.email, password);
   }
 };
