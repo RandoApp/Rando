712dc17117da1ee240b0344fc36f2f223cd2112e
dimhold <dimhold@gmail.com>
2016-09-24 13:28:40 +0300

    Fix user schema ovveriding

--- scripts/migration/1.0.14-to-1.0.15.js
+++ scripts/migration/1.0.14-to-1.0.15.js
@@ -1,6 +1,30 @@
+var mongoose = require("mongoose");
+var winston = require("winston");
 var config = require("config");
 var dbMigration = require("./1.0.14-1.0.15/dbMigration");
 
-require("randoDB").connect(config.db.url);
+var db = {
+  connect (callback) {
+    mongoose.connect(config.db.url);
+    var db = mongoose.connection;
 
-dbMigration.run();
+    db.on("error", function (e) {
+      winston.error("Monodb connection error: " + e);
+    });
+
+    db.on("open", function () {
+      winston.info("Connection to mongodb established");
+      callback();
+    });
+    return db;
+  },
+  disconnect () {
+    mongoose.disconnect();
+    winston.info("Connections to mongodb closed");
+  }
+};
+
+db.connect(function () {
+  dbMigration.run();
+  db.disconnect();
+});
