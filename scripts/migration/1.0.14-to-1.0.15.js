81244f3eb2b2790612e2974f2e7c2796476c5b94
dimhold <dimhold@gmail.com>
2016-09-24 13:15:02 +0300

    Fix module path

--- scripts/migration/1.0.14-to-1.0.15.js
+++ scripts/migration/1.0.14-to-1.0.15.js
@@ -1,5 +1,5 @@
 var config = require("config");
-var dbMigration = require("./migration/1.0.14-1.0.15/dbMigration");
+var dbMigration = require("./1.0.14-1.0.15/dbMigration");
 
 require("randoDB").connect(config.db.url);
 
