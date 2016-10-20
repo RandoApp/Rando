325e7282d0c29422149f2d1c0742d26d4e2c9c2f
dimhold <dimhold@gmail.com>
2016-10-20 15:14:20 +0300

    Add 404 page

--- src/service/shareService.js
+++ src/service/shareService.js
@@ -9,20 +9,25 @@ var s3Service = require("./s3Service");
 var imageService = require("./imageService");
 var fs = require("fs");
 
-var shareRandoHtmlTemplate = fs.readFileSync("static/share.html").toString();
+var shareRandoHtmlTemplate = fs.readFileSync("template/share.template.html").toString();
+var notFoundRandoHtmlTemplate = fs.readFileSync("template/404.template.html").toString();
 
 module.exports = {
   generateHtmlWithRando (randoId, callback)  {
     db.user.getLightRandoByRandoId(randoId, function (err, data) {
       logger.debug("[shareService.generateHtmlWithRando]", "Start rendering html");
-      if (data && data.out[0]) {
-        var html = shareRandoHtmlTemplate.replace("{{RANDO_URL}}", data.out[0].imageURL)
-          .replace("{{MAP_URL}}", data.out[0].mapURL);
-        callback(null, html);
+      var html = "";
+      if (!err && data && data.out[0]) {
+        var rando = data.out[0];
+        html = shareRandoHtmlTemplate
+          .replace("{{RANDO_URL}}", rando.imageURL)
+          .replace("{{MAP_URL}}", rando.mapURL);
       } else {
         logger.warn("[shareService.generateHtmlWithRando]", "Cannot render html because rando is empty:", data);
-        callback("ERROR");
+        html = notFoundRandoHtmlTemplate;
       }
+
+      callback(null, html);
     });
   }
 };
