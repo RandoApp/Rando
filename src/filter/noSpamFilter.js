5fe65bbbfb10d5495fa8437c8a9861df34e2fb94
dimhold <dimhold@gmail.com>
2016-11-21 16:13:39 +0300

    Fix method name

--- src/filter/noSpamFilter.js
+++ src/filter/noSpamFilter.js
@@ -12,14 +12,14 @@ module.exports = {
   run (req, res, next) {
     logger.info("Start noSpamFilter for user:", req.lightUser.email);
     var user = req.lightUser;
-    db.user.getLastLightOutRandosByEmail(user.email, config.app.limit.images, function (err, userWithOut) {
+    db.user.getAllLightOutRandosByEmail(user.email, config.app.limit.images, function (err, userWithOut) {
       if (err) {
         logger.error("[noSpamFilter]", "Cannot fetch last N randos from DB, because:", err);
         return next();
       }
 
       if (!userWithOut || !userWithOut.out) {
-        logger.error("[noSpamFilter]", "User is empty or don't have out:", userWithOut);
+        logger.error("[noSpamFilter]", "User is empty or don't have out:", userWithOutg);
         return next();
       }
 
