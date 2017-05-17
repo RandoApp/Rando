6b40c6c75313d0c84bb9b597a27dc688a00707b3
dimhold <dimhold@gmail.com>
2017-05-17 17:58:16 +0300

    Implement report api

--- src/service/commentService.js
+++ src/service/commentService.js
@@ -4,7 +4,7 @@ var async = require("async");
 var Errors = require("../error/errors");
 
 module.exports = {
-  delete: function (user, randoId, callback) {
+  delete (user, randoId, callback) {
     logger.debug("[commentService.delete, ", user.email, "] Start delete rando: ", randoId);
     async.parallel([
       function deleteFromOut(done) {
@@ -24,5 +24,30 @@ module.exports = {
       logger.debug("[commentService.delete, ", user.email, "] User successfully updated with deleted rando: ", randoId);
       return callback(null, {command: "delete", result: "done"});
     });
+  },
+  report (user, randoId, callback) {
+    logger.debug("[commentService.report, ", user.email, "] Start report rando: ", randoId);
+    async.parallel([
+      reportRando (done) {
+        logger.trace("[commentService.report.reportRando, ", user.email, "]", "reportRando: ", randoId);
+        db.user.updateReportFlagForInRando(user.email, randoId, 1, done);
+      },
+      reportUserByRandoId (done) {
+        logger.trace("[commentService.report.reportUserByRandoId, ", user.email, "]", "reportUserByRandoId: ", randoId);
+        db.user.addReportForUserByRandoId(randoId, {
+          created: Date.now(),
+          reason: "Reported by " + user.email + " because randoId: " + randoId,
+        }, done);
+      }
+    ], function (err) {
+      logger.trace("[commentService.report, ", user.email, "]", "Processing db updated results for rando: ", randoId);
+      if (err) {
+        logger.debug("[commentService.report, ", user.email, "] We have error in DB when updating user with rando: ", randoId, " Error: ", err.message);
+        return callback(Errors.System(err));
+      }
+      logger.debug("[commentService.report, ", user.email, "] User successfully updated with reported rando: ", randoId);
+      return callback(null, {command: "report", result: "done"});
+    });
+
   }
 };
