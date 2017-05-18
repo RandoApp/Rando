8ad5b42addf327f6c7247de7a0d4542a51bf722d
dimhold <dimhold@gmail.com>
2017-05-18 13:05:35 +0300

    Implement ban user

--- src/service/commentService.js
+++ src/service/commentService.js
@@ -1,6 +1,7 @@
 var db = require("randoDB");
 var logger = require("../log/logger");
 var async = require("async");
+var config = require("config");
 var Errors = require("../error/errors");
 
 module.exports = {
@@ -25,29 +26,54 @@ module.exports = {
       return callback(null, {command: "delete", result: "done"});
     });
   },
-  report (user, randoId, callback) {
-    logger.debug("[commentService.report, ", user.email, "] Start report rando: ", randoId);
-    async.parallel({
-      reportRando (done) {
-        logger.trace("[commentService.report.reportRando, ", user.email, "]", "reportRando: ", randoId);
-        db.user.updateReportFlagForInRando(user.email, randoId, 1, done);
-      },
-      reportUserByRandoId (done) {
-        logger.trace("[commentService.report.reportUserByRandoId, ", user.email, "]", "reportUserByRandoId: ", randoId);
-        db.user.addReportForUserByRandoId(randoId, {
-          created: Date.now(),
-          reason: "Reported by " + user.email + " because randoId: " + randoId,
-        }, done);
-      }
-    }, function (err) {
-      logger.trace("[commentService.report, ", user.email, "]", "Processing db updated results for rando: ", randoId);
-      if (err) {
-        logger.debug("[commentService.report, ", user.email, "] We have error in DB when updating user with rando: ", randoId, " Error: ", err.message);
-        return callback(Errors.System(err));
-      }
-      logger.debug("[commentService.report, ", user.email, "] User successfully updated with reported rando: ", randoId);
-      return callback(null, {command: "report", result: "done"});
-    });
+  report (goodUser, reporedRandoId, callback) {
+    logger.debug("[commentService.report, ", goodUser.email, "] Start report rando: ", randoId);
+    if (!goodUser || !goodUser.email || !reporedRandoId) {
+      return callback(Errors.IncorrectArgs());
+    }
 
+    async.waterfall({
+      fetchBadUser (done) {
+        db.user.getLightUserMetaByOutRandoId(reporedRandoId, done);
+      },
+      updateData (badUser, done) {
+        if (!badUser || !badUser.email) {
+          return done(Errors.IncorrectArgs());
+        }
+
+        async.parallel({
+          reportRandoOnGoodUser (parallelDone) {
+            logger.trace("[commentService.report.reportRando, report by: ", goodUser.email, "for user: ", badUser.email, "reportRando: ", reporedRandoId);
+            db.user.updateReportFlagForInRando(goodUser.email, reporedRandoId, 1, parallelDone);
+          },
+          addEventToReportArrayForBadUser (parallelDone) {
+            logger.trace("[commentService.report.reportUserByRandoId, ", goodUser.email, "]", "reportUserByRandoId: ", reporedRandoId);
+            db.user.addReportForUser(badUser.email, {
+              reportedBy: goodUser.email,
+              randoId: reporedRandoId,
+              reportedDate: Date.now(),
+              reason: "Reported by " + goodUser.email + " because randoId: " + reporedRandoId,
+            }, parallelDone);
+          },
+          banBadUserIfNecessary (parallelDone) {
+            if (Array.isArray(badUser.report)) {
+              var users = badUser.report.map(r => r.reportedBy);
+              users.push(goodUser.email);
+              if (new Set(users).size >= config.app.limit.reporedByUsers) {
+                db.user.updateUserMetaByEmail(badUser.email, {ban: config.app.limit.permanentBanTo}, parallelDone);
+              }
+            }
+          }
+        }, done);
+      }}, (err) => {
+        logger.trace("[commentService.report, ", goodUser.email, "]", "Processing db updated results for rando: ", reporedRandoId);
+        if (err) {
+          logger.debug("[commentService.report, ", goodUser.email, "] We have error in DB when updating user with rando: ", reporedRandoId, " Error: ", err.message);
+          return callback(Errors.System(err));
+        }
+        logger.debug("[commentService.report, ", goodUser.email, "] User successfully updated with reported rando: ", reporedRandoId);
+        return callback(null, {command: "report", result: "done"});
+      }
+    );
   }
 };
