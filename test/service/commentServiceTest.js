db5256bc4d6cc67fe9c8bcf7c1ba3b3778627888
Viktar Patotski <xp.vit.blr@gmail.com>
2017-08-09 14:49:19 +0300

    add test

--- test/service/commentServiceTest.js
+++ test/service/commentServiceTest.js
@@ -2,6 +2,7 @@ var should = require("should");
 var sinon = require("sinon");
 var config = require("config");
 var commentService = require("../../src/service/commentService");
+const pushNotificationService = require("../../src/service/pushNotificationService");
 var db = require("randoDB");
 var Errors = require("../../src/error/errors");
 var mockUtil = require("../mockUtil");
@@ -11,6 +12,7 @@ describe("Comment service.", function () {
 
     afterEach(function() {
       mockUtil.clean(db);
+      mockUtil.clean(pushNotificationService);
     });
 
     it("Delete flag should be set to 1 in out by calling db.user.updateOutRandoProperties", function (done) {
@@ -85,7 +87,13 @@ describe("Comment service.", function () {
     var user = {
       email: "user@mail.com",
       out:[{randoId: 123, delete: 0, rating: 0}, {randoId: 456, delete: 0, rating:0}],
-      in:[{randoId: 789, delete: 0, rating: 0}, {randoId: 999, delete: 0, rating : 0}]
+      in:[{randoId: 789, delete: 0, rating: 0, creation: 100,  detected:["bla"], imageSizeURL: "imageSizeURLValue", imageURL: "imageURLValue",mapSizeURL: "mapSizeURLValueIN", mapURL: "mapURLValueIN"  }, {randoId: 999, delete: 0, rating : 0}]
+      };
+
+      var user2 = {
+      email: "user2@mail.com",
+      in:[{randoId: 123, delete: 0, rating: 0}, {randoId: 456, delete: 0, rating:0}],
+      out:[{randoId: 789, delete: 0, rating: 0, creation: 100,  detected:["bla"], imageSizeURL: "imageSizeURLValue", imageURL: "imageURLValue",mapSizeURL: "mapSizeURLValueOUT", mapURL: "mapURLValueOUT"  }, {randoId: 999, delete: 0, rating : 0}]
       };
 
     before((done) => {
@@ -97,8 +105,11 @@ describe("Comment service.", function () {
     });
 
     beforeEach(function(done) {
-      db.user.create(user, done);
-    });
+      db.user.create(user, (err)=>{
+        db.user.create(user2,done);
+      }
+        );
+      });
 
     afterEach((done) => {
       db.user.removeAll(done);
@@ -140,17 +151,47 @@ describe("Comment service.", function () {
 
       commentService.rate(user, 789, 3, (err, response) => {
         should.exist(err);
-        err.should.be.eql(Errors.System(new Error("Some db error")));
+        err.message.should.be.eql("Some db error");
         done();
       });
     });
 
-    it("Rate flag should return Errors.IncorrectArgs when user rates himself", function (done) {
+    it("Should return Errors.IncorrectArgs when user rates himself", function (done) {
       commentService.rate(user, 456, 3, (err, response) => {
         should.exist(err);
         err.should.be.eql(Errors.IncorrectArgs());  
         done();
       });
     });
+
+     it("Should set rating correctly for both users when user rates rando in his IN", function (done) {
+      sinon.stub(pushNotificationService, "sendMessageToAllActiveUserDevices", function (message,stranger, callback) {
+        should.exist(message);
+        message.notificationType.should.be.eql("rated");
+        should.exist(message.rando);
+        should(message).be.eql("");
+        should(message.rando.randoId).be.eql(789);
+        should(message.rando.rating).be.eql(3);
+        should(message.rando.creation).be.eql(100);
+        should(message.rando.imageSizeURL).be.eql("imageSizeURLValue");
+        should(message.rando.imageURL).be.eql("imageURLValue");
+        should(message.rando.detected).be.eql(["bla"]);
+        should(message.rando.mapSizeURL).be.eql("mapSizeURLValueIN");
+        should(message.rando.mapURL).be.eql("mapURLValueIN");
+        should.exist(stranger);
+        stranger.email.should.be.eql("user2@mail.com");
+        callback();
+      });
+
+      commentService.rate(user, 789, 3, (err, response) => {
+        should.not.exist(err);
+        should.exist(response);
+        response.command.should.be.eql("rate");
+        response.result.should.be.eql("done");
+        //db.user.getLightUserWithInAndOutByEmail(user.email, ()
+        done();
+      });
+    });
   });
 });
+
